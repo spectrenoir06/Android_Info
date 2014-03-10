102ed324ea8d3374f1cfb899370e7f93e01e70b3
spectrenoir06 <antoine.doussaud@laposte.net>
2014-03-10 15:52:12 +0100

    test

---
+++ Android_Info/src/main.lua
@@ -0,0 +1,15 @@
+SCREEN_PHONE_HEIGHT = love.graphics.getHeight()
+SCREEN_PHONE_WIDTH  =  love.graphics.getWidth()
+
+function love.load(arg)
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+  love.graphics.print(SCREEN_PHONE_HEIGHT,10,10)
+  
+end
